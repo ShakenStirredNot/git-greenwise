6a0a0fa20be60f85f8f94b91e8679db890508d36
vbhat <vishakabhat@g.ucla.edu>
2023-08-16 18:34:03 -0700

    added functionality for scan button

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'home.dart';
+import 'figma/scan.dart';
+import 'figma/scan_results.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(const MainApp());
+  //runApp(const MainApp());
+  runApp(const HomeApp());
+  //runApp(const ScanApp());
+  //runApp(const ScanResultApp));
 }
-
 
 
--- lib/styles.dart
+++ lib/styles.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
-const kMainFont = 'Roboto';
-const kButtonFont = 'Roboto';
-const kDisplayFont = 'SquadaOne';
+// Greenwise styles
+const gMainFont = 'Proxima Nova';
+const gLightGreen = Color(0xFFE8FAD3);
 
-const kColorGreen = Color(0xFF395144);
+const gTitleTextStyle = TextStyle(
+  fontFamily: gMainFont,
+  fontSize: 30.0,
+  color: Colors.black,
+  fontWeight: FontWeight.w700,
+  height: 1.20,
+  decoration: TextDecoration.none,
+);
+
+const gAnalyzingTextStyle = TextStyle(
+    fontFamily: gMainFont,
+    fontSize: 25.0,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    decoration: TextDecoration.none);
+
+const gResultTextStyle = TextStyle(
+    fontFamily: gMainFont,
+    fontSize: 25.0,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    decoration: TextDecoration.none);
+
+const gResultRatingTextStyle = TextStyle(
+    fontFamily: gMainFont,
+    fontSize: 15.0,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    decoration: TextDecoration.none);
+
+const gButtonTextStyle = TextStyle(
+    fontFamily: gMainFont,
+    fontSize: 20.0,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    decoration: TextDecoration.none);
+
+// Reference styles
+const kColorGreen =  Color(0xFF395144);
 const kColorLightGreen = Color(0XFF4E6C50);
-const kColorBrown = Color(0XFFAA8B56);
-const kColorLightYellow = Color(0xFFF0EBCE);
 
 const kColorRed = Color(0xFFD96666);
 const kColorLightRed = Color(0xFFF2CECE);
@@ -18,30 +54,3 @@ const kColorMayGreen = Color(0xFF6a994e);
 const kColorAndroidGreen = Color(0xFFa7c957);
 const kColorEggshell = Color(0xFFf2e8cf);
 const kColorBitterSweetShimmer = Color(0xFFbc4749);
-
-const kBgColor = kColorGreen;
-
-const kTitleTextStyle = TextStyle(
-  fontFamily: kDisplayFont,
-  fontSize: 50.0,
-  color: kColorAndroidGreen,
-  decoration: TextDecoration.none,
-);
-
-const kAnalyzingTextStyle = TextStyle(
-    fontFamily: kMainFont,
-    fontSize: 25.0,
-    color: kColorEggshell,
-    decoration: TextDecoration.none);
-
-const kResultTextStyle = TextStyle(
-    fontFamily: kDisplayFont,
-    fontSize: 35.0,
-    color: kColorLightYellow,
-    decoration: TextDecoration.none);
-
-const kResultRatingTextStyle = TextStyle(
-    fontFamily: kMainFont,
-    fontSize: 18.0,
-    color: Colors.white,
-    decoration: TextDecoration.none);
